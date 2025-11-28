aa6af7ccd0363db52a008ce8444fd454ca72d405
sangamesh nidode <sangameshnidode58@gmail.com>
2025-11-29 00:10:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -120,6 +120,13 @@ app.get(/^\/api\/.*/, (req, res) => {
 app.get(/^(?!\/api)(?!\/admin)(?!\/dashboard).+$/, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on PORT:", PORT);
+});
+
+
 
 
 
