bc8c69f18993b9d9656e8d3697c3cbacf353b1d7
sangamesh nidode <sangameshnidode58@gmail.com>
2025-11-29 00:34:36 +0530

    Update createAdmin.js

--- createAdmin.js
+++ createAdmin.js
@@ -1,31 +1,24 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
 const Admin = require("./models/Admin");
 
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to Render MongoDB");
 
-async function createAdmin() {
-  const username = "admin";      // CHANGE if you want
-  const password = "admin123";   // CHANGE if you want
+    const hash = await bcrypt.hash("admin123", 10);
 
-  const hashed = await bcrypt.hash(password, 10);
+    await Admin.create({
+      username: "admin",
+      password: hash
+    });
 
-  const admin = new Admin({ username, password: hashed });
-
-  await admin.save();
-
-  console.log("Admin created successfully:");
-  console.log("Username:", username);
-  console.log("Password:", password);
-
-  mongoose.connection.close();
-}
-
-createAdmin();
+    console.log("✨ Admin created successfully");
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit();
+  }
+})();
